8456f41012fc6b68de1c1ea83cffeb898670eff7
Jaspreet Singh <71667958+Singh2je@users.noreply.github.com>
2021-07-09 15:35:46 -0400

    Did Part 3

--- game.js
+++ game.js
@@ -1,5 +1,12 @@
+var config = {
+    width: 256,
+    height: 272,
+    backgroundColor: 0x000000,
+    scene: [Scene1, Scene2]
+}
+
 window.onload = function() {
-    var game = new Phaser.Game();
+    var game = new Phaser.Game(config);
 }
 
 
