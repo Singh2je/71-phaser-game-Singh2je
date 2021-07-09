1f50abd12d6e9c7bccb17eaf685f1bbd162c29c5
Jaspreet Singh <71667958+Singh2je@users.noreply.github.com>
2021-07-09 16:40:17 -0400

    Done with Part 5 Sprites added

--- Scene1.js
+++ Scene1.js
@@ -6,14 +6,35 @@ class Scene1 extends Phaser.Scene {
 
     preload() {
         this.load.image("background", "assets/images/background.png");
-        this.load.image("ship", "assets/images/ship.png");
-        this.load.image("ship2", "assets/images/ship2.png");
-        this.load.image("ship3", "assets/images/ship3.png");
-    }
+       //this.load.image("ship", "assets/images/ship.png");
+        //this.load.image("ship2", "assets/images/ship2.png");
+        //this.load.image("ship3", "assets/images/ship3.png");
 
+        this.load.spritesheet("ship", "assets/spritesheets/ship.png",{
+            frameWidth: 16,
+            frameHeight: 16
+        });
+
+        this.load.spritesheet("ship2", "assets/spritesheets/ship2.png",{
+            frameWidth: 32,
+            frameHeight: 16
+        });
+
+        this.load.spritesheet("ship3", "assets/spritesheets/ship3.png",{
+            frameWidth: 32,
+            frameHeight: 32
+        });
+
+        this.load.spritesheet("explosion", "assets/spritesheets/explosion.png",{
+            frameWidth: 16,
+            frameHeight: 16
+        });
+
+    }
     create() {
         this.add.text(20, 20, "Loading Game...");
         this.scene.start("playGame");
     }
 
 }
+  
--- Scene2.js
+++ Scene2.js
@@ -8,12 +8,54 @@ class Scene2 extends Phaser.Scene {
         this.background = this.add.tileSprite(0,0, config.width, config.height, "background");
         this.background.setOrigin(0,0);
 
-        this.ship1 = this.add.image(config.width/2 - 50, config.height/2, "ship");
-        this.ship2 = this.add.image(config.width/2, config.height/2, "ship2");
-        this.ship3 = this.add.image(config.width/2 + 50, config.height/2, "ship3");
+        this.ship1 = this.add.sprite(config.width/2 - 50, config.height/2, "ship");
+        this.ship2 = this.add.sprite(config.width/2, config.height/2, "ship2");
+        this.ship3 = this.add.sprite(config.width/2 + 50, config.height/2, "ship3");
 
-        this.add.text(20, 20, "Playing Game", {font: "25px Arial", fill: "yellow"});
+        this.anims.create({
+            key: "ship1_anim",
+            frames: this.anims.generateFrameNumbers("ship"),
+            frameRate: 20,
+            repeat: -1
+        });
         
+        this.anims.create({
+            key: "ship2_anim",
+            frames: this.anims.generateFrameNumbers("ship2"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "ship3_anim",
+            frames: this.anims.generateFrameNumbers("ship3"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "explode",
+            frames: this.anims.generateFrameNumbers("explosion"),
+            frameRate: 20,
+            repeat: 0,
+            hideOnComplete: true
+        });
+
+        this.add.text(20, 20, "Playing Game", {
+            font: "25px Arial",
+            fill: "yellow"
+        });
+        
+        this.ship1.play("ship1_anim");
+        this.ship2.play("ship2_anim");
+        this.ship3.play("ship3_anim");
+
+        this.ship1.setInteractive();
+        this.ship2.setInteractive();
+        this.ship3.setInteractive();
+
+        this.input.on('gameobjectdown', this.destroyShip, this);
+  
     }
 
         moveShip(ship, speed) {
@@ -29,6 +71,11 @@ class Scene2 extends Phaser.Scene {
             ship.x = randomX;
         }
         
+        destroyShip(pointer, gameObject){
+            gameObject.setTexture("explosion");
+            gameObject.play("explode");
+        }
+
         update() {
             this.moveShip(this.ship1, 1);
             this.moveShip(this.ship2, 2);
