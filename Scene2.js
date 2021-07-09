9b067b1b5a9edb83ce3ce1cec9b9fd031c0fafed
Jaspreet Singh <71667958+Singh2je@users.noreply.github.com>
2021-07-09 15:50:23 -0400

    Completed Part 4

--- Scene2.js
+++ Scene2.js
@@ -5,7 +5,7 @@ class Scene2 extends Phaser.Scene {
     }
 
     create() {
-        this.background = this.add.image(0,0,"background");
+        this.background = this.add.tileSprite(0,0, config.width, config.height, "background");
         this.background.setOrigin(0,0);
 
         this.ship1 = this.add.image(config.width/2 - 50, config.height/2, "ship");
@@ -13,5 +13,27 @@ class Scene2 extends Phaser.Scene {
         this.ship3 = this.add.image(config.width/2 + 50, config.height/2, "ship3");
 
         this.add.text(20, 20, "Playing Game", {font: "25px Arial", fill: "yellow"});
+        
     }
+
+        moveShip(ship, speed) {
+            ship.y += speed;
+            if (ship.y > config.height){
+                this.resetShipPos(ship);
+            }
+        }
+
+        resetShipPos(ship){
+            ship.y = 0;
+            var randomX = Phaser.Math.Between(0, config.width);
+            ship.x = randomX;
+        }
+        
+        update() {
+            this.moveShip(this.ship1, 1);
+            this.moveShip(this.ship2, 2);
+            this.moveShip(this.ship3, 3);
+
+            this.background.tilePositionY -= 0.5;
+        }
 }
--- game.js
+++ game.js
@@ -2,7 +2,8 @@ var config = {
     width: 256,
     height: 272,
     backgroundColor: 0x000000,
-    scene: [Scene1, Scene2]
+    scene: [Scene1, Scene2],
+    pixelArt: true
 }
 
 window.onload = function() {
