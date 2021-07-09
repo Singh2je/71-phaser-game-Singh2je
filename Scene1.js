9289bae1fd511e054f6d566d37c2af3b4fb4c65c
Jaspreet Singh <71667958+Singh2je@users.noreply.github.com>
2021-07-09 17:03:01 -0400

    Done with Part 6 - physics

--- Scene1.js
+++ Scene1.js
@@ -30,6 +30,12 @@ class Scene1 extends Phaser.Scene {
             frameHeight: 16
         });
 
+        
+        this.load.spritesheet("power-up", "assets/spritesheets/power-up.png",{
+            frameWidth: 16,
+            frameHeight: 16
+        });
+
     }
     create() {
         this.add.text(20, 20, "Loading Game...");
--- Scene2.js
+++ Scene2.js
@@ -41,6 +41,51 @@ class Scene2 extends Phaser.Scene {
             hideOnComplete: true
         });
 
+        this.anims.create({
+            key: "red",
+            frames: this.anims.generateFrameNumbers("power-up", {
+            start: 0,
+            end: 1
+            }),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "gray",
+            frames: this.anims.generateFrameNumbers("power-up", {
+            start: 2,
+            end: 3
+            }),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.powerUps = this.physics.add.group();
+
+        var maxObjects = 4;
+        for (var i = 0; i <= maxObjects; i++) {
+            var powerUp = this.physics.add.sprite(16, 16, "power-up");
+            this.powerUps.add(powerUp);
+            powerUp.setRandomPosition(0, 0, config.width, config.height);
+
+            
+            if (Math.random() > 0.5) {
+                powerUp.play("red");
+            }
+            else{
+                powerUp.play("gray");
+            }   
+
+            powerUp.setVelocity(100, 100);
+            powerUp.setCollideWorldBounds(true);
+            powerUp.setBounce(1);
+        }
+
+
+        
+
+
         this.add.text(20, 20, "Playing Game", {
             font: "25px Arial",
             fill: "yellow"
